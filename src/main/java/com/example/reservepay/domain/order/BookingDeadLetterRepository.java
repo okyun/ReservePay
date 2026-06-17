6a2c2d3908e1dcf94d78312af03320f3f2eb8c64
okyun <53889980+okyun@users.noreply.github.com>
2026-06-18 04:59:09 +0900

    이동한 파일
    - 하위 파일로 이동, IdempotencyKeyStore 파일 삭제

--- src/main/java/com/example/reservepay/domain/order/BookingDeadLetterRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.reservepay.domain.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingDeadLetterRepository extends JpaRepository<BookingDeadLetter, Long> {
-}
--- src/main/java/com/example/reservepay/domain/payment/strategy/PaymentFailureType.java
+++
@@ -1,6 +0,0 @@
-package com.example.reservepay.domain.payment.strategy;
-
-public enum PaymentFailureType {
-    PERMANENT,
-    TRANSIENT
-}
